9d9d24ab0da03c9e8ff4e10883bf2eeca063cb4a
Damien <hello@neiwad.io>
2021-03-13 10:17:58 -0500

    add netlify CMS

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,7 +1,27 @@
 module.exports = {
   siteName: "NEIWAD",
   siteUrl: "https://neiwad.io",
-  plugins: [],
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "posts/**/*.md",
+        typeName: "Post",
+        remark: {
+          plugins: [
+            // ...local plugins
+          ],
+        },
+      },
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`,
+        modulePath: `src/admin/index.js`,
+      },
+    },
+  ],
   templates: {
     Project: [
       {
@@ -9,5 +29,15 @@ module.exports = {
         component: "./src/templates/Project.vue",
       },
     ],
+  },
+  transformers: {
+    remark: {
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      anchorClassName: "icon icon-link",
+      plugins: [
+        // ...global plugins
+      ],
+    },
   },
 };
